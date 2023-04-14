9fac1d78627ddcf2cb663cd0d317c80cb24d2ea4
jaeseong-kim <doctorwho123@naver.com>
2023-04-14 19:00:05 +0900

    Initial commit

---
+++ src/main/java/com/eyeteaboard/eyeteaboard/EyeTeaBoardApplication.java
@@ -0,0 +1,13 @@
+package com.eyeteaboard.eyeteaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeTeaBoardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EyeTeaBoardApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/eyeteaboard/eyeteaboard/EyeTeaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyeteaboard.eyeteaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyeTeaBoardApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

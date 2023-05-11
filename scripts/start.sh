046feebf19d78a2a1134b816c08e6166103fb658
jaeseong-kim <doctorwho123@naver.com>
2023-05-11 16:10:36 +0900

    Feat: 자동화 ci/cd 기능 추가

---
+++ scripts/start.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+REPOSITORY=/home/ubuntu/eyeteaboard/step3
+PROJECT_NAME=eyeteaboard
+
+echo "> Build 파일 복사"
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1) # -t는 최신 수정순으로 정렬, -r은 반대로 정렬
+
+echo "> JAR name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+IDLE_PROFILE=$(find_idle_profile)
+
+echo "> $JAR_NAME를 profile-$IDLE_PROFILE 로 실행합니다."
+
+nohup java -jar \
+  -Dspring.config.location=classpath:/application.properties,/home/ubuntu/eyeteaboard/application-mail.properties,\
+/home/ubuntu/eyeteaboard/application-oauth.properties,/home/ubuntu/eyeteaboard/application-real-db.properties,classpath:/application-$IDLE_PROFILE.properties \
+-Dspring.profiles.active=$IDLE_PROFILE \
+$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
